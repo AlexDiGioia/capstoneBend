f7335763d2b721fb459b3d5559d27aad180b1ddd
AlexDiGioia <digioia.alex2001@gmail.com>
2024-10-01 15:22:06 +0200

    Initial commit

---
+++ src/main/java/alexdigioia/capstoneBend/CapstoneBendApplication.java
@@ -0,0 +1,13 @@
+package alexdigioia.capstoneBend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alexdigioia/capstoneBend/CapstoneBendApplicationTests.java
@@ -0,0 +1,13 @@
+package alexdigioia.capstoneBend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneBendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
