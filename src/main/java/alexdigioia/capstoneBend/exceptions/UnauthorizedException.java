b4de9d28eb892d1c761bdef437ef390de715f961
AlexDiGioia <digioia.alex2001@gmail.com>
2024-10-03 16:54:23 +0200

    config

---
+++ src/main/java/alexdigioia/capstoneBend/exceptions/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package alexdigioia.capstoneBend.exceptions;
+
+public class UnauthorizedException extends RuntimeException {
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
